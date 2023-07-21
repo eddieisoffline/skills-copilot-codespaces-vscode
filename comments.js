e7b61772b0ede32fbae7f4470633f89496345e12
eddieisoffline <116452710+eddieisoffline@users.noreply.github.com>
2023-07-21 02:00:09 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,56 @@
+// Create web server with node.js
+// Create file server.js
+// Run node server.js to start the server
+// Go to http://localhost:3000/ to see the server
+// Create a file called index.html
+// In server.js, add this code:
+//   var http = require('http');
+//   var fs = require('fs');
+//   http.createServer(function (req, res) {
+//     fs.readFile('index.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }).listen(3000);
+// Run node server.js
+// Go to http://localhost:3000/ to see the server
+// Create a file called index.html
+// In server.js, add this code:
+//   var http = require('http');
+//   var fs = require('fs');
+//   http.createServer(function (req, res) {
+//     fs.readFile('index.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }).listen(3000);
+// Run node server.js
+// Go to http://localhost:3000/ to see the server
+// Create a file called index.html
+// In server.js, add this code:
+//   var http = require('http');
+//   var fs = require('fs');
+//   http.createServer(function (req, res) {
+//     fs.readFile('index.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }).listen(3000);
+// Run node server.js
+// Go to http://localhost:3000/ to see the server
+// Create a file called index.html
+// In server.js, add this code:
+//   var http = require('http');
+//   var fs = require('fs');
+//   http.createServer(function (req, res) {
+//     fs.readFile('index.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }).listen(3000);
+// Run node server.js
+// Go to http://localhost:300
